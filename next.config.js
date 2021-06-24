e60741e44e12a012400c60e2579b6de33157d4e9
Kaio Dadalton <kaiofreitas.dadalton@gmail.com>
2021-06-23 22:44:32 -0300

    Feat: adds .eslintignore for config files.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const baseUrl = '';
+
+module.exports = withBundleAnalyzer({
+  webpack5: true,
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: baseUrl,
+  env: {
+    baseUrl: baseUrl,
+  },
+  
+});
